9005f560f9600d872d21f805185cf8319b65e6a8
ashley-hansen <ashley.hansen@cqumail.com>
2024-05-04 23:48:02 +1000

    basic client conection to server co-ordinator establised

--- src/main/java/com/coit13229ass2/ServerCoordinator.java
+++ src/main/java/com/coit13229ass2/ServerCoordinator.java
@@ -1,7 +1,67 @@
 
 package com.coit13229ass2;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+
 
 public class ServerCoordinator {
-    
+    public static void main(String[] args){
+        try {
+            //Set Socket port number
+            int serverPort = 7611;
+            int serverBookPort = 7622;
+            int serverMoviePort = 7633;
+            ServerSocket listenSocket = new ServerSocket(serverPort);
+            //create connection object utilising thread for multiple concurrent connections
+            while (true) {
+                Socket clientSocket = listenSocket.accept();
+                ClientConnection connection = new ClientConnection(clientSocket);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }//End Main method
+
+}//End of class
+
+class ClientConnection extends Thread {
+    //initialise in and out data streams
+    ObjectInputStream input;
+    ObjectOutputStream output;
+    Socket clientSocket;
+
+    public ClientConnection(Socket aClientSocket) {
+
+        try {
+            clientSocket = aClientSocket;
+            input = new ObjectInputStream(clientSocket.getInputStream());
+            output = new ObjectOutputStream(clientSocket.getOutputStream());
+            //starts server thread
+            this.start();
+        } catch (IOException e) {
+            System.out.println("Connection:" + e.getMessage());
+        }
+
+    }
+    //run method starts thread
+    public void run() {
+
+        boolean running = true;
+
+        System.out.println("Hello World!");
+
+  
+    }
+
 }
